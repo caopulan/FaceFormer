8ef4791ced4a929cb31859cc21129b64a6a268bf
sariel <2788787973@qq.com>
2022-05-21 15:55:45 +0800

    tmp

--- wav2vec.py
+++ wav2vec.py
@@ -9,6 +9,8 @@ from transformers.modeling_outputs import BaseModelOutput
 from typing import Optional, Tuple
 _CONFIG_FOR_DOC = "Wav2Vec2Config"
 
+# ------------
+
 # the implementation of Wav2Vec2Model is borrowed from https://huggingface.co/transformers/_modules/transformers/models/wav2vec2/modeling_wav2vec2.html#Wav2Vec2Model
 # initialize our encoder with the pre-trained wav2vec 2.0 weights.
 def _compute_mask_indices(
